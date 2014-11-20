202836a573bed04cf800db380dcc4db88edad48d
Pranit <pranit_anand@hotmail.com>
2014-11-20 09:58:11 +0100

    SecondCA4

--- server/routes/REST_api.js
+++ server/routes/REST_api.js
@@ -97,4 +97,24 @@ router.get('/wikiTitle/titles', function(req, res) {
   });
 });
 
+router.get('/wiki/getTitles', function(req, res) {
+  if(typeof global.mongo_error !== "undefined"){
+    res.status(500);
+    res.end("Error: "+global.mongo_error+" To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)");
+    return;
+  }
+
+  wiki.getTitles( function (err, titles) {
+    if (err) {
+      res.status(err.status || 400);
+      res.end(JSON.stringify({error: err.toString()}));
+      return;
+    }
+    res.header("Content-type","application/json");
+    res.end(JSON.stringify(titles));
+  });
+});
+
+
+
 module.exports = router;
